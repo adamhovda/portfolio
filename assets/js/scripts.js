b2bd4414f693aded76efc43abe395ce868ab7333
adam hovda <adamhovda@gmail.com>
2023-05-28 16:49:30 -0700

    initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,18 @@
+
+// jiggle animation when you scroll
+// const observer = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//       const square = entry.target.querySelector('.jiggle');
+      
+  
+//       if (entry.isIntersecting) {
+//         square.classList.add('jiggle-animation');
+//         return; // if we added the class, exit the function
+//       }
+  
+//       // We're not intersecting, so remove the class!
+//       square.classList.remove('jiggle-animation');
+//     });
+//   });
+  
+//   observer.observe(document.querySelector('.tech'));
